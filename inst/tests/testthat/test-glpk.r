ddaa7588604e15a677f0efe4a5ed287270ff7f97
Jaak Simm <jaak.simm@gmail.com>
2014-06-21 14:36:00 +0200

    second test

--- inst/tests/testthat/test-glpk.r
+++ inst/tests/testthat/test-glpk.r
@@ -12,3 +12,17 @@ test_that("GLPK example 1", {
   expect_equal( s1$solution, c(0, 10.2, 9.9), tolerance=1e-5 )
   expect_equal( s1$status, 0 )
 })
+
+test_that("GLPK example 2", {
+  obj <- c(2, 4, 3)
+  mat <- matrix(c(3, 2, 1, 4, 1, 3, 2, 2, 9), nrow = 3)
+  dir <- c("<=", "<=", "<=")
+  rhs <- c(60.6, 30, 60.6)
+  max <- TRUE
+
+  s1 = Rglpk_solve_LP(obj, mat, dir, rhs, max = max)
+  expect_equal( s1$optimum, 62.62, tolerance=1e-5 )
+  expect_equal( s1$solution, c(0, 14.14, 2.02), tolerance=1e-5 )
+  expect_equal( s1$status, 0 )
+})
+
