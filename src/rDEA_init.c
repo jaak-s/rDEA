e06109b652c4d6fbc5f44374cb715c6672f1b4b6
Jaak Simm <jaak.simm@gmail.com>
2020-02-05 23:11:54 +0100

    added compile symbols

---
+++ src/rDEA_init.c
@@ -0,0 +1,25 @@
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME: 
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .C calls */
+extern void multi_glp_solve(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void rDEA_get_engine_version(void *);
+extern void rDEA_initialize();
+
+static const R_CMethodDef CEntries[] = {
+    {"multi_glp_solve",         (DL_FUNC) &multi_glp_solve,         30},
+    {"rDEA_get_engine_version", (DL_FUNC) &rDEA_get_engine_version,  1},
+    {"rDEA_initialize",         (DL_FUNC) &rDEA_initialize,          0},
+    {NULL, NULL, 0}
+};
+
+void R_init_rDEA(DllInfo *dll)
+{
+    R_registerRoutines(dll, CEntries, NULL, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
+
